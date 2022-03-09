bcce1f848cfd178e5e29f699565ad62f31872374
AncutaBuhas <ancuta_buhas@yahoo.com>
2022-03-09 16:33:02 +0200

    Add deleteBook + json Request photo

--- src/main/java/library/rest/BookController.java
+++ src/main/java/library/rest/BookController.java
@@ -3,10 +3,7 @@ package library.rest;
 import library.entity.Book;
 import library.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +21,10 @@ public class BookController {
     @GetMapping(value = "/book/all")
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
+    }
+
+    @DeleteMapping(value = "/book/{id}")
+    public void deleteBook(@PathVariable Long id){
+        bookRepository.deleteById(id);
     }
 }
